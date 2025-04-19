b3b652a97203d278822e3944d0c5c73978fe625b
krishnaprasad <krishnaprasadpa2001@gmail.com>
2025-04-19 11:36:33 +0000

    Added IP validation code

--- IP.sh
+++ IP.sh
@@ -2,6 +2,7 @@
 
 is_private_ip() {
 	ip="$2"
+	checkIpValidity($2)
 	# Check for private IP ranges
 	if [[ "$ip" =~ ^10\. || \
 		"$ip" =~ ^192\.168\. || \
@@ -10,4 +11,16 @@ is_private_ip() {
 		else
 			echo "IP is public"
 	fi
+}
+
+checkIpValidity(){
+    local ip="$1"
+    local octet="(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])"
+    local pattern="^$octet\\.$octet\\.$octet\\.$octet$"
+
+    if [[ "$ip" =~ $pattern ]]; then
+        echo "Valid IPv4 address: $ip"
+    else
+        echo "Invalid IPv4 address: $ip"
+    fi
 }
